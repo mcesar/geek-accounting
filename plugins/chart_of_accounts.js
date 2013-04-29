58fd695cba489fdc59a49db833b4e48e32f76792
mcesarhm <mcesarhm@gmail.com>
2013-04-29 18:02:11 -0300

    Done with balance sheet (Card #5)

--- plugins/chart_of_accounts.js
+++ plugins/chart_of_accounts.js
@@ -401,6 +401,66 @@ Service.prototype.ledger = function (coaId, accountId, from, to, callback) {
 	});
 };
 
+Service.prototype.balanceSheet = function (coaId, at, callback) {
+	var that = this;
+	db.conn().collection('transactions_' + coaId, function (err, collection) {
+		if (err) { return callback(err); }
+		collection.find({ date: { $lte: at } }).
+			sort({ date: 1, timestamp: 1 }).
+			toArray(function (err, items) {
+				var props = [ 'debits', 'credits' ], result = {}, entries= [], entry, 
+					balanceItem, i, j, k;
+				for (i = 0; i < items.length; i += 1) {
+					for (j = 0; j < props.length; j += 1) {
+						for (k = 0; k < items[i][props[j]].length; k += 1) {
+							entry = items[i][props[j]][k];
+							if (!result[entry.account.toString()]) {
+								balanceItem = { 
+									account: entry.account,
+									value: 0,
+									balanceIncrements: [] 
+								};
+								result[entry.account.toString()] = balanceItem;
+								entries.push(balanceItem);
+							}
+							result[entry.account.toString()].balanceIncrements.push({
+								prop: props[j],
+								value: entry.value
+							});
+						}
+					}
+				}
+				(function eachItem (i) {
+					if (i >= entries.length) { 
+						entries = entries.sort(function (a, b) {
+							return a.localeCompare(b);
+						});
+						return callback(null, entries) 
+					};
+					that.account(entries[i].account.toString(), function (err, account) {
+						var j;
+						if (err) { return callback(err); }
+						for (j = 0; j < entries[i].balanceIncrements.length; j++) {
+							result[account._id.toString()].value += 
+								that.balanceIncrement(account, 
+									entries[i].balanceIncrements[j], 
+									entries[i].balanceIncrements[j].prop);
+						}
+						delete entries[i].balanceIncrements;
+						entries[i].account = {
+							_id: account._id,
+							number: account.number,
+							account: account.name,
+							debitBalance: account.debitBalance,
+							creditBalance: account.creditBalance
+						};
+						eachItem(i + 1);
+					});
+				}(0));
+			});
+	});
+};
+
 function chartsOfAccounts (req, res, next) {
 	new Service().chartsOfAccounts(function (err, items) {
 		if (err) { return next(err); }
@@ -511,6 +571,17 @@ function ledger (req, res, next) {
 	);
 }
 
+function balanceSheet (req, res, next) {
+	var at = new Date(req.query.at);
+	if (isNaN(at.getTime())) {
+		return next(new Error("The 'at' date parameter must be informed"));
+	}
+	new Service().balanceSheet(req.params.id, at, function (err, balanceSheet) {
+		if (err) { return next(err); }
+		res.send(balanceSheet);
+	});
+}
+
 exports.Service = Service;
 
 exports.setup = function (app) {
@@ -525,4 +596,5 @@ exports.setup = function (app) {
 	app.post('/charts-of-accounts/:id/transactions', addTransaction);
 	app.get('/charts-of-accounts/:id/journal', journal);
 	app.get('/charts-of-accounts/:id/accounts/:accountId/ledger', ledger);
+	app.get('/charts-of-accounts/:id/balance-sheet', balanceSheet);
 };
