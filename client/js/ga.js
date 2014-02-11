eeae93c8cd3a106ec0a21fa2d90b08fda3f91dd3
marcosco <marcos-cesar.oliveira@planejamento.gov.br>
2014-02-11 11:01:17 -0200

    Adds a page for adding transactions on HTML client

--- client/js/ga.js
+++ client/js/ga.js
@@ -3,7 +3,8 @@ angular.module('ga.service', ['ngRoute','ngResource'])
   return $resource('/charts-of-accounts', {}, {
     chartsOfAccounts: {method:'GET', params:{}, isArray:true},
     balanceSheet: {method:'GET', params:{}, isArray:true, url: '/charts-of-accounts/:coa/balance-sheet?at=:at'},
-    incomeStatement: {method:'GET', params:{}, url: '/charts-of-accounts/:coa/income-statement?from=:from&to=:to'}
+    incomeStatement: {method:'GET', params:{}, url: '/charts-of-accounts/:coa/income-statement?from=:from&to=:to'},
+    addTransaction: {method:'POST', params:{}, url: '/charts-of-accounts/:coa/transactions'}
   });
 }]);
 
@@ -18,6 +19,10 @@ angular.module('ga', ['ngRoute','ngResource', 'ga.service'])
       controller:'IsCtrl',
       templateUrl:'income-statement.html'
     })
+    .when('/charts-of-accounts/:coa/transaction', {
+      controller:'TransactionCtrl',
+      templateUrl:'transaction.html'
+    })
     .when('/login', {
       controller:'LoginCtrl',
       templateUrl:'login.html'
@@ -25,6 +30,19 @@ angular.module('ga', ['ngRoute','ngResource', 'ga.service'])
     .otherwise({
       redirectTo:'/'
     });
+})
+.directive('gaFocus', function() {
+  return {
+    scope: { trigger: '=gaFocus' },
+    link: function(scope, element) {
+      scope.$watch('trigger', function(value) {
+        if(value === true) { 
+          element[0].focus();
+          scope.trigger = false;
+        }
+      });
+    }
+  };
 });
 
 var LoginCtrl = function ($scope, $rootScope, $http, $location, GaServer) {
@@ -32,7 +50,7 @@ var LoginCtrl = function ($scope, $rootScope, $http, $location, GaServer) {
     $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa($scope.user + ':' + $scope.password);
     $http.get('/ping').success(function(data, status, headers, config) {
       $rootScope.loggedIn = true;
-      $location.path($rootScope.previousPath);
+      $location.path($rootScope.previousPath).replace();
     }).error(function(data, status, headers, config) {
       $http.defaults.headers.common['Authorization'] = undefined;
       $scope.errorMessage = "Login ou senha incorretos";
@@ -47,6 +65,14 @@ var NavigatorCtrl = function ($scope, $rootScope, $location, $http, GaServer) {
   }
   $scope.isLoggedIn = function() {
     return $rootScope.loggedIn === true;
+  };
+  $scope.actionLabel = function() {
+    switch (lastSegment()) {
+    case 'transaction':
+      return ': lançamento';
+    default:
+      return '';
+    }
   };
   $scope.$watch(function() { return $location.path(); }, function(newValue, oldValue) {
     if (!$scope.isLoggedIn()) {
@@ -143,3 +169,75 @@ var IsCtrl = function ($scope, $routeParams, GaServer) {
     }
   );
 };
+
+var TransactionCtrl = function ($scope, $routeParams, $window, GaServer) {
+  var add = function (callback) {
+    if ($scope.transaction.debits.length < 1 || $scope.transaction.credits.length < 1) {
+      $scope.errorMessage = "Devem ser informados pelo menos um débito e pelo menos um crédito";
+      return;
+    }
+    if (!$scope.date) {
+      $scope.errorMessage = "A data deve ser informada";
+      return;
+    }
+    if (!$scope.transaction.memo) {
+      $scope.errorMessage = "O memorando deve ser informado";
+      return;
+    }
+    $scope.transaction.date = $scope.date + 'T00:00:00Z';
+    GaServer.addTransaction({coa: $routeParams.coa}, $scope.transaction, function () {
+      if (callback) callback();
+    }, function (response) {
+      $scope.errorMessage = response.data.replace('Error: ', '');
+    });
+  };
+  var clear = function () {
+    $scope.transaction = {debits:[], credits:[]};
+    $scope.entries = [];
+    $scope.date = new Date().toISOString().substring(0, 10);    
+  };
+  clear();
+  $scope.add = function () {
+    add(function () { $window.history.back(); });
+  };
+  $scope.addAndContinue = function () {
+    add(function () { clear(); });
+  };
+  $scope.addEntry = function () {
+    var entry;
+    if (!$scope.account) {
+      $scope.errorMessage = "A conta deve ser informada";
+      return;
+    }
+    if (!$scope.debit && !$scope.credit) {
+      $scope.errorMessage = "Ou o débito ou o crédito deve ser informado";
+      return;
+    }
+    if ($scope.debit) {
+      entry = {e: {account: $scope.account, value: $scope.debit}, debit: $scope.debit};
+      $scope.transaction.debits.push(entry.e)
+    } else {
+      entry = {e: {account: $scope.account, value: $scope.credit}, credit: $scope.credit};
+      if ($scope.credit) {
+        $scope.transaction.credits.push(entry.e)
+      }
+    }
+    $scope.entries.push(entry);
+    $scope.account = $scope.debit = $scope.credit = undefined;
+    $scope.errorMessage = undefined;
+  };
+  $scope.debitsSum = function () {
+    var result = 0, i = 0;
+    for (; i < $scope.transaction.debits.length; i += 1) {
+      result += $scope.transaction.debits[i].value;
+    }
+    return result;
+  };
+  $scope.creditsSum = function () {
+    var result = 0, i = 0;
+    for (; i < $scope.transaction.credits.length; i += 1) {
+      result += $scope.transaction.credits[i].value;
+    }
+    return result;
+  };
+}
