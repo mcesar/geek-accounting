836016c76729d2ed48ed739c72390143727c1bdc
Marcos César de Oliveira <mcesarhm@gmail.com>
2014-02-19 22:01:48 -0300

    Implements loading indicator

--- client/js/ga.js
+++ client/js/ga.js
@@ -26,6 +26,47 @@ angular.module('ga.service', ['ngRoute','ngResource'])
     updateUser: {method:'PUT', params:{}, url:'/users/:user'},
     removeUser: {method:'DELETE', params:{}, url:'/users/:user'},
   });
+})
+.factory('httpInterceptor', function ($q, $rootScope, $log) {
+
+  var numLoadings = 0;
+
+  return {
+    request: function (config) {
+      numLoadings++;
+      // Show loader
+      $rootScope.$broadcast("loader_show");
+      return config || $q.when(config)
+
+    },
+    response: function (response) {
+      if ((--numLoadings) === 0) {
+        // Hide loader
+        $rootScope.$broadcast("loader_hide");
+      }
+      return response || $q.when(response);
+    },
+    responseError: function (response) {
+      if (!(--numLoadings)) {
+        // Hide loader
+        $rootScope.$broadcast("loader_hide");
+      }
+      return $q.reject(response);
+    }
+  };
+})
+.config(function ($httpProvider) {
+  $httpProvider.interceptors.push('httpInterceptor');
+})
+.directive("loader", function ($rootScope) {
+  return function ($scope, element, attrs) {
+    $scope.$on("loader_show", function () {
+        return element.show();
+    });
+    return $scope.$on("loader_hide", function () {
+      return element.hide();
+    });
+  };
 });
 
 angular.module('ga', ['ngRoute','ngResource', 'ga.service'])
