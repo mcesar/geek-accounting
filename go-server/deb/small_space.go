ff4f0c35a9d02684ad328d18a4682f87b6b10fc9
Marcos César de Oliveira <mcesarhm@gmail.com>
2015-05-12 21:21:43 -0300

    Add Projection method implementation

--- go-server/deb/small_space.go
+++ go-server/deb/small_space.go
@@ -1,5 +1,7 @@
 package deb
 
+import xmath "github.com/mcesarhm/geek-accounting/go-server/extensions/math"
+
 type smallSpace struct {
 	arr                      Array
 	dateOffset, momentOffset int
@@ -20,11 +22,77 @@ func (ss *smallSpace) Append(s Space) {
 }
 
 func (ss *smallSpace) Slice(a []Account, d []DateRange, m []MomentRange) Space {
-	return nil
+	result := smallSpace{ss.arr.Copy(), ss.dateOffset, ss.momentOffset}
+	x, y, z := ss.arr.Dimensions()
+	for i := 0; i < x; i++ {
+		for j := 0; j < y; j++ {
+			for k := 0; k < z; k++ {
+				if !containsDate(d, Date(j+1+ss.dateOffset)) ||
+						!containsMoment(m, Moment(k+1+ss.momentOffset)) {
+					result.arr[i][j][k] = 0
+				}
+			}
+		}
+	}
+	for j := 0; j < y; j++ {
+		for k := 0; k < z; k++ {
+			found := false
+			for i := 0; i < x; i++ {
+				if containsAccount(a, Account(i+1)) && result.arr[i][j][k] != 0 {
+					found = true
+					break
+				}
+			}
+			if !found {
+				for i := 0; i < x; i++ {
+					result.arr[i][j][k] = 0
+				}				
+			}
+		}
+	}
+	return &result
 }
 
 func (ss *smallSpace) Projection(a []Account, d []DateRange, m []MomentRange) Space {
-	return nil
+	result := smallSpace{ss.arr.Copy(), ss.dateOffset, ss.momentOffset}
+	x, y, z := ss.arr.Dimensions()
+	for i := 0; i < x; i++ {
+		for _, each_dr := range d {
+			for _, each_mr := range m {
+				sum := int64(0)
+				j_start := xmath.Min(int(each_dr.Start-1-Date(ss.dateOffset)), y)
+				j_end := xmath.Min(int(each_dr.End-Date(ss.dateOffset)), y)
+				k_start := xmath.Min(int(each_mr.Start-1-Moment(ss.momentOffset)), z)
+				k_end := xmath.Min(int(each_mr.End-Moment(ss.momentOffset)), z)
+				for j := j_start; j < j_end ; j++ {
+					for k := k_start; k < k_end; k++ {
+						sum += result.arr[i][j][k]
+					}
+				}
+				result.arr[i][j_start][k_start] = sum
+			}
+		}
+	}
+	for j := 0; j < y; j++ {
+		for k := 0; k < z; k++ {
+			found := false
+			if containsDateStart(d, Date(j+1+ss.dateOffset)) && 
+					containsMomentStart(m, Moment(k+1+ss.momentOffset)) {
+				for i := 0; i < x; i++ {
+					if containsAccount(a, Account(i+1)) && result.arr[i][j][k] != 0 {
+						found = true
+						break
+					}
+				}
+			}
+			if !found {
+				for i := 0; i < x; i++ {
+					result.arr[i][j][k] = 0
+				}				
+			}
+		}
+	}
+	return &result
 }
 
 func (ss *smallSpace) Transactions() chan *Transaction {
@@ -49,4 +117,49 @@ func (ss *smallSpace) Transactions() chan *Transaction {
 		close(out)
 	}()
 	return out
+}
+
+func containsAccount(a []Account, elem Account) bool {
+	for _, each := range a {
+		if each == elem {
+			return true
+		}
+	}
+	return false
+}
+
+func containsDate(d []DateRange, elem Date) bool {
+	for _, each := range d {
+		if each.Start <= elem && each.End >= elem {
+			return true
+		}
+	}
+	return false
+}
+
+func containsMoment(m []MomentRange, elem Moment) bool {
+	for _, each := range m {
+		if each.Start <= elem && each.End >= elem {
+			return true
+		}
+	}
+	return false
+}
+
+func containsDateStart(d []DateRange, elem Date) bool {
+	for _, each := range d {
+		if each.Start == elem {
+			return true
+		}
+	}
+	return false
+}
+
+func containsMomentStart(m []MomentRange, elem Moment) bool {
+	for _, each := range m {
+		if each.Start == elem {
+			return true
+		}
+	}
+	return false
 }
--- go-server/deb/small_space_test.go
+++ go-server/deb/small_space_test.go
@@ -14,22 +14,56 @@ func TestNewSmallSpace(t *testing.T) {
 		Transaction{Moment(1), Date(1), Entries{Account(1): 1, Account(2): -1}},
 		Transaction{Moment(3), Date(2), Entries{Account(1): 1, Account(2): -1}},
 	}
-	assert(t, spaces, cases)
+	assertTransactions(t, spaces, cases)
 }
 
 func TestAppend(t *testing.T) {
 	spaces := []Space{
 		NewSmallSpace(Array{{{1, -1}}}.Transposed()),
+	}
+	arguments := []Space{
 		NewSmallSpaceWithOffset(Array{{{2, -2}}}.Transposed(), 0, 1),
-		NewSmallSpaceWithOffset(Array{{{1, -1}}, {{2, -2}}}.Transposed(), 0, 0),
 	}
-	spaces[0].Append(spaces[1])
-	if !reflect.DeepEqual(spaces[0], spaces[2]) {
-		t.Errorf("%v != %v", spaces[0], spaces[2])
+	cases := []Space{
+		NewSmallSpace(Array{{{1, -1}}, {{2, -2}}}.Transposed()),
 	}
+	spaces[0].Append(arguments[0])
+	assertSpaces(t, spaces, cases)
 }
 
-func assert(t *testing.T, spaces []Space, cases []Transaction) {
+func TestSlice(t *testing.T) {
+	spaces := []Space{
+		NewSmallSpace(Array{{{1, -1}}, {{2, -2}}}.Transposed()),
+	}
+	arguments := []struct{a []Account; d []DateRange; m []MomentRange}{
+		{[]Account{Account(1),Account(2)},[]DateRange{DateRange{1,1}},[]MomentRange{MomentRange{2,2}}},
+	}
+	cases := []Space{
+		NewSmallSpace(Array{{{0, 0}}, {{2, -2}}}.Transposed()),
+	}
+	for i := range spaces {
+		spaces[i] = spaces[i].Slice(arguments[i].a, arguments[i].d, arguments[i].m)
+	}
+	assertSpaces(t, spaces, cases)
+}
+
+func TestProjection(t *testing.T) {
+	spaces := []Space{
+		NewSmallSpace(Array{{{1, -1}}, {{2, -2}}}.Transposed()),
+	}
+	arguments := []struct{a []Account; d []DateRange; m []MomentRange}{
+		{[]Account{Account(1),Account(2)},[]DateRange{DateRange{1,1}},[]MomentRange{MomentRange{1,2}}},
+	}
+	cases := []Space{
+		NewSmallSpace(Array{{{3, -3}}, {{0, 0}}}.Transposed()),
+	}
+	for i := range spaces {
+		spaces[i] = spaces[i].Projection(arguments[i].a, arguments[i].d, arguments[i].m)
+	}
+	assertSpaces(t, spaces, cases)
+}
+
+func assertTransactions(t *testing.T, spaces []Space, cases []Transaction) {
 	i := 0
 	for _, s := range spaces {
 		for tx := range s.Transactions() {
@@ -41,5 +75,13 @@ func assert(t *testing.T, spaces []Space, cases []Transaction) {
 	}
 	if i < len(cases) {
 		t.Errorf("len(transactions):%v != len(cases):%v", i, len(cases))
+	}
+}
+
+func assertSpaces(t *testing.T, spaces []Space, cases []Space) {
+	for i, s := range spaces {
+		if !reflect.DeepEqual(s, cases[i]) {
+			t.Errorf("%v != %v", s, cases[i])
+		}
 	}
 }
