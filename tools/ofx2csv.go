f52b9e95db11ab23bc368f43da99fc2639d0af01
Marcos César de Oliveira <mcesarhm@gmail.com>
2015-08-13 19:04:56 -0300

    Add options to inform a range of values for substitutions in ofx2csv tool

--- tools/ofx2csv.go
+++ tools/ofx2csv.go
@@ -6,21 +6,26 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"mcesar.io/ofx"
 )
 
 type substitution struct {
-	pattern string
-	account string
-	memo    string
+	pattern  string
+	account  string
+	memo     string
+	minValue float64
+	maxValue float64
 }
 
 func main() {
 	substitutionsFile := flag.String("s", "", "substitutions file")
+	filename := flag.String("f", "", "file name")
 	flag.Parse()
 
 	substitutions := []substitution{}
@@ -39,11 +44,32 @@ func main() {
 				fmt.Fprintln(os.Stderr, "reading substitutions file:", err)
 				return
 			}
-			substitutions = append(substitutions, substitution{record[0], record[1], record[2]})
+			s := substitution{record[0], record[1], record[2], 0, 0}
+			if len(record) > 3 {
+				if s.minValue, err = strconv.ParseFloat(record[3], 64); err != nil {
+					fmt.Fprintln(os.Stderr, "Error converting minValue:", err)
+				}
+			}
+			if len(record) > 4 {
+				if s.maxValue, err = strconv.ParseFloat(record[4], 64); err != nil {
+					fmt.Fprintln(os.Stderr, "Error converting maxValue:", err)
+				}
+			}
+			substitutions = append(substitutions, s)
+		}
+	}
+	var f *os.File
+	if *filename == "" {
+		f = os.Stdin
+	} else {
+		var err error
+		if f, err = os.Open(*filename); err != nil {
+			fmt.Fprintln(os.Stderr, "Error opening file:", err)
+			return
 		}
 	}
 
-	doc, err := ofx.Parse(os.Stdin)
+	doc, err := ofx.Parse(f)
 	if err != nil {
 		log.Fatal("Error parsing OFX: ", err)
 		os.Exit(1)
@@ -55,6 +81,12 @@ func main() {
 		t.Memo = st(sl(t.Memo))
 		for _, s := range substitutions {
 			if matched, err := regexp.MatchString(sl(s.pattern), sl(t.Memo)); matched && err == nil {
+				if s.minValue > 0 && math.Abs(t.Amount) < s.minValue {
+					continue
+				}
+				if s.maxValue > 0 && math.Abs(t.Amount) > s.maxValue {
+					continue
+				}
 				account = s.account
 				t.Memo = s.memo
 				break
