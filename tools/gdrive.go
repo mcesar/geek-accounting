f2c4ff6c9e23cef70f7ca479c599b51259f75d25
Marcos César de Oliveira <mcesarhm@gmail.com>
2015-08-23 12:10:24 -0300

    Add tool to download pdfs from Google Drive

---
+++ tools/gdrive.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/drive/v2"
+)
+
+func main() {
+	folderId := flag.String("f", "", "Folder Id")
+	secretFile := flag.String("s", "", "Secret file")
+	flag.Parse()
+
+	ctx := context.Background()
+
+	b, err := ioutil.ReadFile(*secretFile)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	config, err := google.ConfigFromJSON(b, drive.DriveScope)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(ctx, config)
+
+	srv, err := drive.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve drive Client %v", err)
+	}
+
+	files, err := FilesInFolder(srv, *folderId)
+	if err != nil {
+		log.Fatalf("Unable to retrieve files.", err)
+	}
+	tempFolder, err := ioutil.TempDir("", "ga-tools")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		if f.MimeType == "application/pdf" && f.DownloadUrl != "" {
+			if err := DownloadFile(client, f, tempFolder); err != nil {
+				log.Fatalln(err)
+			}
+			fmt.Printf("%s\n", filepath.Join(tempFolder, f.Title))
+		}
+	}
+}
+
+// getClient uses a Context and Config to retrieve a Token
+// then generate a Client. It returns the generated Client.
+func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
+	cacheFile, err := tokenCacheFile()
+	if err != nil {
+		log.Fatalf("Unable to get path to cached credential file. %v", err)
+	}
+	tok, err := tokenFromFile(cacheFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(cacheFile, tok)
+	}
+	return config.Client(ctx, tok)
+}
+
+// getTokenFromWeb uses Config to request a Token.
+// It returns the retrieved Token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var code string
+	if _, err := fmt.Scan(&code); err != nil {
+		log.Fatalf("Unable to read authorization code %v", err)
+	}
+
+	tok, err := config.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web %v", err)
+	}
+	return tok
+}
+
+// tokenCacheFile generates credential file path/filename.
+// It returns the generated credential path/filename.
+func tokenCacheFile() (string, error) {
+	usr, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
+	os.MkdirAll(tokenCacheDir, 0700)
+	return filepath.Join(tokenCacheDir,
+		url.QueryEscape("drive-api-quickstart.json")), err
+}
+
+// tokenFromFile retrieves a Token from a given file path.
+// It returns the retrieved Token and any read error encountered.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	t := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(t)
+	defer f.Close()
+	return t, err
+}
+
+// saveToken uses a file path to create a file and store the
+// token in it.
+func saveToken(file string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", file)
+	f, err := os.Create(file)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func FilesInFolder(d *drive.Service, folderId string) ([]*drive.File, error) {
+	var fs []*drive.File
+	pageToken := ""
+	for {
+		q := d.Files.List().Q(fmt.Sprintf("'%v' in parents", folderId))
+		// If we have a pageToken set, apply it to the query
+		if pageToken != "" {
+			q = q.PageToken(pageToken)
+		}
+		r, err := q.Do()
+		if err != nil {
+			fmt.Printf("An error occurred: %v\n", err)
+			return fs, err
+		}
+		fs = append(fs, r.Items...)
+		pageToken = r.NextPageToken
+		if pageToken == "" {
+			break
+		}
+	}
+	return fs, nil
+}
+
+func DownloadFile(client *http.Client, f *drive.File, folder string) error {
+	res, err := client.Get(f.DownloadUrl)
+	if err != nil {
+		return fmt.Errorf("An error occurred: %v", err)
+	}
+	defer res.Body.Close()
+
+	outFile, err := os.Create(filepath.Join(folder, f.Title))
+	if err != nil {
+		return fmt.Errorf("An error occurred: %v\n", err)
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, res.Body)
+	if err != nil {
+		return fmt.Errorf("An error occurred: %s", err)
+	}
+	return nil
+}
