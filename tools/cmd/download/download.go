1dd405a85d7f5311bb5e67acf569988a32a3f3a9
Marcos César de Oliveira <mcesarhm@gmail.com>
2017-11-09 20:52:58 -0200

    Download folders too

--- tools/cmd/download/download.go
+++ tools/cmd/download/download.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 	"path/filepath"
 
 	"google.golang.org/api/drive/v2"
@@ -28,24 +30,32 @@ func main() {
 		log.Fatalf("Unable to retrieve drive Client %v", err)
 	}
 
-	files, err := gdrive.FilesInFolder(srv, *folderId)
-	if err != nil {
-		log.Fatalf("Unable to retrieve files.", err)
-	}
 	if *destination == "" {
 		*destination, err = ioutil.TempDir("", "ga-tools")
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
+
+	dowloadFilesInFolder(client, srv, *folderId, *destination)
+}
+
+func dowloadFilesInFolder(client *http.Client, srv *drive.Service, folderId, destination string) {
+	files, err := gdrive.FilesInFolder(srv, folderId)
+	if err != nil {
+		log.Fatalf("Unable to retrieve files.", err)
+	}
 	for _, f := range files {
 		if (f.MimeType == "application/pdf" ||
 			f.MimeType == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") &&
 			f.DownloadUrl != "" {
-			if err := gdrive.DownloadFile(client, f, *destination); err != nil {
+			if err := gdrive.DownloadFile(client, f, destination); err != nil {
 				log.Fatalln(err)
 			}
-			fmt.Printf("%s\n", filepath.Join(*destination, f.Title))
+			fmt.Printf("%s\n", filepath.Join(destination, f.Title))
+		} else if f.MimeType == "application/vnd.google-apps.folder" {
+			os.Mkdir(destination+"/"+f.Title, os.ModeDir)
+			dowloadFilesInFolder(client, srv, f.Id, destination+"/"+f.Title)
 		}
 	}
 }
